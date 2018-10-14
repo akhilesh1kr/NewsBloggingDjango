60c93963ad92faccadc39a8bb742c3399c7a1630
Akhilesh Kumar <akhilesh1kr@gmail.com>
2018-10-14 14:45:03 +0530

    Module Added for - Showing links of similar posts on post_details

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Count
+
 from django.views.generic import ListView
 
 from django.shortcuts import render, get_object_or_404
@@ -70,12 +72,19 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+        .order_by('-same_tags','-publish')[:4]
     return render(request, 
                 'blog/post/detail.html',
                 {'post': post,
                 'comments': comments,
                 'new_comment': new_comment,
-                'comment_form': comment_form
+                'comment_form': comment_form,
+                'similar_posts': similar_posts
                 })
 
 def post_share(request, post_id):
